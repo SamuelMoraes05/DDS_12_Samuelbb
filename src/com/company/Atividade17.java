38de6426bd3a86832708a5fbc4e19b5dfcd90710
Samuel Moraes <102255886+SamuelMoraes05@users.noreply.github.com>
2022-03-30 14:59:19 -0300

    Update Atividade17.java

--- src/com/company/Atividade17.java
+++ src/com/company/Atividade17.java
@@ -4,31 +4,46 @@ import java.util.Scanner;
 
 public class Atividade17 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Qual o tamnho, em metros quadrados, da área desejada: ");
+        System.out.print("Qual o tamanho, em metros quadrados, da área desejada: ");
         double tamanhoEmMetrosQuadrados = scanner.nextDouble();
-        byte quantidadeDeLitrosPorMetros = 3;
-        byte quantidadeDeLataporTinta  = 18;
+        byte quantidadeDeLitrosPorMetros = 6;
+        byte quantidadeDeLataporTinta = 18;
         byte preçoDaLata = 80;
 
-        double corbeturaDaTintaEmLata = tamanhoEmMetrosQuadrados / quantidadeDeLitrosPorMetros;
-        double quantidadeDeLata = corbeturaDaTintaEmLata / quantidadeDeLataporTinta;
+        double coberturaDaTintaEmLata = tamanhoEmMetrosQuadrados / quantidadeDeLitrosPorMetros;
+        double quantidadeDeLata = coberturaDaTintaEmLata / quantidadeDeLataporTinta;
         double quantidadePagaPorLata = quantidadeDeLata * preçoDaLata;
 
-        double quantidadeDeTintaPorGalao = 3.6;
+        double quantidadeDeTintaporGalao = 3.6;
         int preçoDoGalao = 25;
 
-        double corbeturaDatintaPorGalao = tamanhoEmMetrosQuadrados / quantidadeDeTintaPorGalao;
-        double quantodadeDeGlalao = corbeturaDatintaPorGalao / quantidadeDeTintaPorGalao;
-        double quatidadePagaPorGalao = quantodadeDeGlalao * preçoDoGalao;
+        double coberturaDaTintaEmGalao = tamanhoEmMetrosQuadrados / quantidadeDeTintaporGalao;
+        double quantidadeDeMetrosQuadradosQueUmGalaoPinta = quantidadeDeTintaporGalao * quantidadeDeLitrosPorMetros;
+        double quantidadeDeGalao = tamanhoEmMetrosQuadrados / quantidadeDeMetrosQuadradosQueUmGalaoPinta;
+        double quantidadePagaPorGalao = quantidadeDeGalao * preçoDoGalao;
 
-        double quanntidadePagaPorLataArredondado = Math.ceil(quantidadePagaPorLata);
-        double quantodadeDeGalaoArredondado = Math.ceil(quatidadePagaPorGalao);
+        double quantidadePagaPorLataArredondado = Math.ceil(quantidadePagaPorLata);
+        double quantidadePagaPorGalaoArrendado = Math.ceil(quantidadePagaPorGalao);
+
+        System.out.println("O preço pago em latas é: " + quantidadePagaPorLataArredondado);
+        System.out.println("A quantidade de lata é: " + quantidadeDeLata);
+        System.out.println("O preço pago em galão é: " + quantidadePagaPorGalaoArrendado);
+        System.out.println("A quantidade de galão é: " + quantidadeDeGalao);
+
+        double misturado = coberturaDaTintaEmGalao + coberturaDaTintaEmLata;
+
+        double misturadoLata =  coberturaDaTintaEmLata / misturado;
+        double misturadoLataArredondado = Math.ceil(misturadoLata);
+        double restoDivisao = misturado % misturadoLata;
+        double misturadoGalao = restoDivisao / quantidadePagaPorGalaoArrendado;
+        double misturadoGalaoArredondado = Math.ceil(misturadoGalao);
 
 
 
-
+        System.out.println("A quantidade de lata usada quando se mistura os dois é: " + misturadoLataArredondado);
+        System.out.println("A quantidade de galão usada quando se mistura os dois é: " + misturadoGalaoArredondado);
+        scanner.close();
     }
 }

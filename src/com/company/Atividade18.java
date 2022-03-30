e25a662491df11e14feff811a6b74b55addd03e7
Samuel Moraes <102255886+SamuelMoraes05@users.noreply.github.com>
2022-03-30 14:58:00 -0300

    Update Atividade18.java

--- src/com/company/Atividade18.java
+++ src/com/company/Atividade18.java
@@ -1,2 +1,16 @@
-package com.company;public class Atividade18 {
+package com.company;
+
+import java.util.Scanner;
+
+public class Atividade18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual o tamanho do arquivo em mMB que deseja baixar? ");
+        double tamnhoArquivo = scanner.nextDouble();
+        System.out.print(" E a velocidade, em MBps, da conexão que deseja baixar? ");
+        double velocidadedInternet = scanner.nextDouble();
+        double tempoDeDownload = tamnhoArquivo / (velocidadedInternet / 8) / 60;
+        System.out.printf("O tempo necessário para download do arquivo é: %.2f minutos.", tempoDeDownload);
+        scanner.close();
+    }
 }
